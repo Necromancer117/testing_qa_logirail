e8b2aeece88754c0790e1d97b5d39e101ef264ac
Albert Juan Rubio <99891284+Necromancer117@users.noreply.github.com>
2024-05-06 10:45:15 +0200

    fix
    stuff

--- Java-Kata/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java-Kata/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -41,281 +41,72 @@ class GildedRoseTest {
 			() -> assertEquals(80, app.items[0].quality, "Quality")
 			);
     }
-    @Nested
-	@DisplayName("Standard")
-    class Standard{
+    
+    @ParameterizedTest(name = "Caso {index} fecha {0} calidad {1}.")
+    @CsvSource({
     	
-    	@Test
-    	@DisplayName("Con calidad y fecha, se queda a 0")
-        void standard_CP1 () {
-        	Item[] items = new Item[] { new Item(STANDARD, 1, 1) };
-            GildedRose app = new GildedRose(items);
+            "1, 1, 0, 0",
+            "22, 2, 21, 1",
+            "34, 0, 33, 0",
+            //"53, 1, 53, 1",
+            "0, 1, -1, 0",
+            "0, 3, -1, 1",
+            "-24, 0, -25, 0",
+            //"-31, 1, -31, 1",
             
-            app.updateQuality();
             
-            assertNotNull(items);
-    		assertAll("Proiedades", 
-    			() -> assertEquals(STANDARD, app.items[0].name, "name"),
-    			() -> assertEquals(0, app.items[0].sellIn, "Sellin"),
-    			() -> assertEquals(0, app.items[0].quality, "Quality")
-    			);
-        }
-    	
-    	@Test
-    	@DisplayName("Con calidad y fecha, calidad final>0")
-        void standard_CP2 () {
-        	Item[] items = new Item[] { new Item(STANDARD, 22, 2) };
-            GildedRose app = new GildedRose(items);
-            
-            app.updateQuality();
-            
-            assertNotNull(items);
-    		assertAll("Proiedades", 
-    			() -> assertEquals(STANDARD, app.items[0].name, "name"),
-    			() -> assertEquals(21, app.items[0].sellIn, "Sellin"),
-    			() -> assertEquals(1, app.items[0].quality, "Quality")
-    			);
-        }
-    	
-    	@Test
-    	@DisplayName("sin calidad y con fecha")
-        void standard_CP3 () {
-        	Item[] items = new Item[] { new Item(STANDARD, 34, 0) };
-            GildedRose app = new GildedRose(items);
-            
-            app.updateQuality();
-            
-            assertNotNull(items);
-    		assertAll("Proiedades", 
-    			() -> assertEquals(STANDARD, app.items[0].name, "name"),
-    			() -> assertEquals(33, app.items[0].sellIn, "Sellin"),
-    			() -> assertEquals(0, app.items[0].quality, "Quality")
-    			);
-        }
-    	
-    	@Test
-    	@DisplayName("Error: calidad negativa")
-        void standard_CP4 () {
-        	Item[] items = new Item[] { new Item(STANDARD, 53, -1) };
-            GildedRose app = new GildedRose(items);
-            
-            app.updateQuality();
-            
-            assertNotNull(items);
-    		assertAll("Proiedades", 
-    			() -> assertEquals(STANDARD, app.items[0].name, "name"),
-    			() -> assertEquals(51, app.items[0].sellIn, "Sellin"), //52?
-    			() -> assertEquals(-1, app.items[0].quality, "Quality")
-    			);
-        }
-    	
-    	@Test
-    	@DisplayName("con calidad sin fecha, calidad final = 0")
-        void standard_CP5 () {
-        	Item[] items = new Item[] { new Item(STANDARD, 0, 1) };
-            GildedRose app = new GildedRose(items);
-            
-            app.updateQuality();
-            
-            assertNotNull(items);
-    		assertAll("Proiedades", 
-    			() -> assertEquals(STANDARD, app.items[0].name, "name"),
-    			() -> assertEquals(-1, app.items[0].sellIn, "Sellin"),
-    			() -> assertEquals(0, app.items[0].quality, "Quality")
-    			);
-        }
-    	
-    	@Test
-    	@DisplayName("con calidad sin fecha")
-        void standard_CP6 () {
-        	Item[] items = new Item[] { new Item(STANDARD, 0, 3) };
-            GildedRose app = new GildedRose(items);
-            
-            app.updateQuality();
-            
-            assertNotNull(items);
-    		assertAll("Proiedades", 
-    			() -> assertEquals(STANDARD, app.items[0].name, "name"),
-    			() -> assertEquals(-1, app.items[0].sellIn, "Sellin"),
-    			() -> assertEquals(1, app.items[0].quality, "Quality")
-    			);
-        }
-    	
-    	@Test
-    	@DisplayName("sin calidad y fecha negativa")
-        void standard_CP7 () {
-        	Item[] items = new Item[] { new Item(STANDARD, -24, 0) };
-            GildedRose app = new GildedRose(items);
-            
-            app.updateQuality();
-            
-            assertNotNull(items);
-    		assertAll("Proiedades", 
-    			() -> assertEquals(STANDARD, app.items[0].name, "name"),
-    			() -> assertEquals(-25, app.items[0].sellIn, "Sellin"),
-    			() -> assertEquals(0, app.items[0].quality, "Quality")
-    			);
-        }
-    	
-    	@Test
-    	@DisplayName("Error: Fecha y calidad negativa")
-        void standard_CP8 () {
-        	Item[] items = new Item[] { new Item(STANDARD, -31, -1) };
-            GildedRose app = new GildedRose(items);
-            
-            app.updateQuality();
-            
-            assertNotNull(items);
-    		assertAll("Proiedades", 
-    			() -> assertEquals(STANDARD, app.items[0].name, "name"),
-    			() -> assertEquals(-31, app.items[0].sellIn, "Sellin"),  //-32??
-    			() -> assertEquals(-1, app.items[0].quality, "Quality")
-    			);
-        }
-    	
+    })
+    @DisplayName("Standard Item")
+    void StandardItem(int initialSellIn, int initialQuality, int expectedSellIn, int expectedQuality) {
+        Item[] items = new Item[] { new Item(STANDARD, initialSellIn, initialQuality) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertNotNull(items);
+        assertAll("Propiedades",
+                () -> assertEquals(STANDARD, app.items[0].name, "name"),
+                () -> assertEquals(expectedSellIn, app.items[0].sellIn, "Sellin"),
+                () -> assertEquals(expectedQuality, app.items[0].quality, "Quality")
+        );
     }
     
-    @Nested
-	@DisplayName("Aged Brie")
-    class Brie{
+    @ParameterizedTest(name = "Caso {index} fecha {0} calidad {1}.")
+    @CsvSource({
     	
-    	@Test
-    	@DisplayName("Con fecha y calidad<50, se queda a 50")
-        void standard_CP1 () {
-        	Item[] items = new Item[] { new Item(BRIE, 1, 49) };
-            GildedRose app = new GildedRose(items);
+            "1, 49, 0, 50",
+            "1, 48, 0, 49",
+            "1, 50, 0, 50",
+            //"40, 82, 40, 82",
+            "0, 49, -1, 50",
+            "0, 47, -1, 49",
+            "0, 50, -1, 50",
+            //"0, 51, 0, 51",
             
-            app.updateQuality();
             
-            assertNotNull(items);
-    		assertAll("Proiedades", 
-    			() -> assertEquals(BRIE, app.items[0].name, "name"),
-    			() -> assertEquals(0, app.items[0].sellIn, "Sellin"),
-    			() -> assertEquals(50, app.items[0].quality, "Quality")
-    			);
-        }
-    	
-    	@Test
-    	@DisplayName("Con fecha y calidad<50")
-        void standard_CP2 () {
-        	Item[] items = new Item[] { new Item(BRIE, 1, 48) };
-            GildedRose app = new GildedRose(items);
-            
-            app.updateQuality();
-            
-            assertNotNull(items);
-    		assertAll("Proiedades", 
-    			() -> assertEquals(BRIE, app.items[0].name, "name"),
-    			() -> assertEquals(0, app.items[0].sellIn, "Sellin"),
-    			() -> assertEquals(49, app.items[0].quality, "Quality")
-    			);
-        }
-    	
-    	@Test
-    	@DisplayName("Con fecha y calidad=50, se queda en 50")
-        void standard_CP3 () {
-        	Item[] items = new Item[] { new Item(BRIE, 1, 50) };
-            GildedRose app = new GildedRose(items);
-            
-            app.updateQuality();
-            
-            assertNotNull(items);
-    		assertAll("Proiedades", 
-    			() -> assertEquals(BRIE, app.items[0].name, "name"),
-    			() -> assertEquals(0, app.items[0].sellIn, "Sellin"),
-    			() -> assertEquals(50, app.items[0].quality, "Quality")
-    			);
-        }
-    	
-    	@Test
-    	@DisplayName("ERROR: Calidad>50")
-        void standard_CP4 () {
-        	Item[] items = new Item[] { new Item(BRIE, 40, 82) };
-            GildedRose app = new GildedRose(items);
-            
-            app.updateQuality();
-            
-            assertNotNull(items);
-    		assertAll("Proiedades", 
-    			() -> assertEquals(BRIE, app.items[0].name, "name"),
-    			() -> assertEquals(40, app.items[0].sellIn, "Sellin"),
-    			() -> assertEquals(82, app.items[0].quality, "Quality")
-    			);
-        }
-    	
-    	@Test
-    	@DisplayName("Sin fecha y calidad<50, final 50")
-        void standard_CP5 () {
-        	Item[] items = new Item[] { new Item(BRIE, 0, 49) };
-            GildedRose app = new GildedRose(items);
-            
-            app.updateQuality();
-            
-            assertNotNull(items);
-    		assertAll("Proiedades", 
-    			() -> assertEquals(BRIE, app.items[0].name, "name"),
-    			() -> assertEquals(-1, app.items[0].sellIn, "Sellin"),
-    			() -> assertEquals(50, app.items[0].quality, "Quality")
-    			);
-        }
-    	
-    	@Test
-    	@DisplayName("Sin fecha y calidad<50")
-        void standard_CP6 () {
-        	Item[] items = new Item[] { new Item(BRIE, 0, 47) };
-            GildedRose app = new GildedRose(items);
-            
-            app.updateQuality();
-            
-            assertNotNull(items);
-    		assertAll("Proiedades", 
-    			() -> assertEquals(BRIE, app.items[0].name, "name"),
-    			() -> assertEquals(-1, app.items[0].sellIn, "Sellin"),
-    			() -> assertEquals(49, app.items[0].quality, "Quality")
-    			);
-        }
-    	
-    	@Test
-    	@DisplayName("Sin fecha y calidad=50")
-        void standard_CP7 () {
-        	Item[] items = new Item[] { new Item(BRIE, 0, 50) };
-            GildedRose app = new GildedRose(items);
-            
-            app.updateQuality();
-            
-            assertNotNull(items);
-    		assertAll("Proiedades", 
-    			() -> assertEquals(BRIE, app.items[0].name, "name"),
-    			() -> assertEquals(-1, app.items[0].sellIn, "Sellin"),
-    			() -> assertEquals(50, app.items[0].quality, "Quality")
-    			);
-        }
-    	
-    	@Test
-    	@DisplayName("Error: Sin fecha y calidad>50")
-        void standard_CP8 () {
-        	Item[] items = new Item[] { new Item(BRIE, 0, 51) };
-            GildedRose app = new GildedRose(items);
-            
-            app.updateQuality();
-            
-            assertNotNull(items);
-    		assertAll("Proiedades", 
-    			() -> assertEquals(BRIE, app.items[0].name, "name"),
-    			() -> assertEquals(0, app.items[0].sellIn, "Sellin"),
-    			() -> assertEquals(51, app.items[0].quality, "Quality")
-    			);
-        }
-    	
-    }
-     	
+    })
+    @DisplayName("Aged Brie")
+    void AgedBrie(int initialSellIn, int initialQuality, int expectedSellIn, int expectedQuality) {
+        Item[] items = new Item[] { new Item(BRIE, initialSellIn, initialQuality) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertNotNull(items);
+        assertAll("Propiedades",
+                () -> assertEquals(BRIE, app.items[0].name, "name"),
+                () -> assertEquals(expectedSellIn, app.items[0].sellIn, "Sellin"),
+                () -> assertEquals(expectedQuality, app.items[0].quality, "Quality")
+        );
+    }  
+
     	@ParameterizedTest(name = "Caso {index} fecha {0} calidad {1}.")
         @CsvSource({
         	
                 "11, 49, 10, 50",
                 "6, 49, 5, 50",
                 "1, 49, 0, 50",
-                "0, 51, 0, 51",
+                //"0, 51, 0, 51",
                 "22, 50, 21, 50",
                 "7, 50, 6, 50",
                 "4, 50, 3, 50",
@@ -324,7 +115,7 @@ class GildedRoseTest {
                 "8, 4, 7, 6",
                 "2, 42, 1, 45",
                 "-567, 47, -568, 0",
-                "62, 51, 62, 51",
+                //"62, 51, 62, 51",
                 
                 
         })
@@ -349,11 +140,11 @@ class GildedRoseTest {
                 "1, 2, 0, 0",
                 "22, 7, 21, 5",
                 "34, 0, 33, 0",
-                "53, -1, 53, -1",
+ //               "53, -1, 53, -1",
                 "0, 4, -1, 0",
                 "-33, 5, -34, 1",
                 "-24, 0, -25, 0",
-                "-31, -1, -31, -1",    
+ //               "-31, -1, -31, -1",    
                 
         })
         @DisplayName("Summoned")
